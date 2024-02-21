ed5d668886fe4f48c8929e651148f386fc6bf886
teamofroman <rsateam@yandex.ru>
2024-02-21 21:29:41 +0300

    Init django project

---
+++ backend/urls.py
@@ -0,0 +1,33 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Yandex Ambassador CRM API',
+        default_version='v1',
+        description='Описание API endpoint for Yandex Ambassador',
+        contact=openapi.Contact(email='admin@kittygram.ru'),
+        license=openapi.License(name='MIT License'),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='docs')
+]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT
+    )
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
