b618e53acc6652194ecffbd0559feb8c010497aa
Дмитрий <dcheredov35@yandex.ru>
2024-02-28 23:54:08 +0500

    исправил замечания_4 28.02

--- ambassador/models.py
+++ ambassador/models.py
@@ -120,6 +120,25 @@ class Ambassador(models.Model):
         verbose_name="Учебное заведение",
         max_length=50,
     )
+    first_name = models.CharField(
+        verbose_name="Имя",
+        max_length=50,
+    )
+    last_name = models.CharField(
+        verbose_name="Фамилия",
+        max_length=50
+    )
+    middle_name = models.CharField(
+        verbose_name="Отчество",
+        max_length=50
+    )
+    sex = models.CharField(
+        verbose_name="Пол",
+        max_length=50
+    )
+    birthday = models.DateField(
+        verbose_name="Дата рождения"
+    )
 
     class Meta:
         verbose_name = 'Амбасадор'
