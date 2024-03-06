9a2907f906905c5a7d4f5734622b7bf01ec3c465
Роман Александров <rsateam@yandex.ru>
2024-03-06 12:18:48 +0300

    Добавлена пагинация

--- api/view_ambassadors.py
+++ api/view_ambassadors.py
@@ -4,6 +4,8 @@ from rest_framework import mixins, viewsets
 from ambassador.models import Ambassador
 from ambassador.serializers import AmbassadorSerializer
 
+from .paginators import CustomPNPaginator
+
 # from rest_framework.decorators import action
 # from rest_framework.response import Response
 
@@ -11,3 +13,4 @@ from ambassador.serializers import AmbassadorSerializer
 class AmbassadorViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     queryset = Ambassador.objects.all()
     serializer_class = AmbassadorSerializer
+    pagination_class = CustomPNPaginator
