a6daaca3f7c4920fa0fcaa38d5dc95ac3b8e0630
teamofroman <rsateam@yandex.ru>
2024-03-10 23:25:08 +0300

    Исправлен код возврата при успешном создании телеграм пользователя

--- api/view_telegram.py
+++ api/view_telegram.py
@@ -14,4 +14,4 @@ class TelegramUserCreateView(mixins.CreateModelMixin, viewsets.GenericViewSet):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_201_CREATED)
